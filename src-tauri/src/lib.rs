eaaf85f6980792167b1fbf5f58f80c89d2173420
Steven Junop <stevenjunop@gmail.com>
2026-06-02 07:07:28 -0400

    Add PTY, vibrancy, notification, regex, tokio deps

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_notification::init())
         .setup(|_app| Ok(()))
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
